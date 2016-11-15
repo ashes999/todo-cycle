7c452f11611dc46f6abc3498c58253564dd06a72
ashes999 <ashes999@yahoo.com>
2016-11-15 15:15:18 -0500

    Sortable (albeit hideous) tasks.

--- TodoCycle.Web/Scripts/angular/TasksApp.js
+++ TodoCycle.Web/Scripts/angular/TasksApp.js
@@ -1,10 +1,17 @@
 ﻿'use strict';
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.sortable']);
 
 angular.module('app').controller('TasksController', ['$scope', '$http',
 function ($scope, $http) {
     var self = this;
+
+    // Configure ui.sortable
+    $scope.sortableOptions = {
+        stop: function (e, ui) {
+            console.log("You just re-ordered some stuff, eh? " + JSON.stringify(self.tasks));
+        },
+    };
 
     $http({ method: 'GET', url: 'api/Task/GetAll' }).success(function (data, status, headers, config) {
         self.tasks = data;
