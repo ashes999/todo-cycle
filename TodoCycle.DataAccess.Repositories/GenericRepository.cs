72d9570a07b7e4616fc2dda228df8c838397c1e4
ashes999 <ashes999@yahoo.com>
2017-04-20 12:46:22 -0400

    Repository and task controller Ninject for DI Generic repository for now (Dapper seems pretty awesome now)

---
+++ TodoCycle.DataAccess.Repositories/GenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace TodoCycle.DataAccess.Repositories
+{
+    // Dapper's pretty smart these days. I don't even have to write SQL any more.
+    // We can devolve this into something domain-object specific if necessary.
+    public class GenericRepository : IGenericRepository
+    {
+        private readonly string connectionString;
+
+        public GenericRepository(ConnectionStringSettings connectionString)
+        {
+            this.connectionString = connectionString.ConnectionString;
+        }
+
+        public IEnumerable<T> Query<T>(string sql, object parameters)
+        {
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                connection.Open();
+                return connection.Query<T>(sql, parameters);
+            }
+        }
+    }
+}
---
+++ TodoCycle.DataAccess.Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoCycle.DataAccess.Repositories
+{
+    public interface IGenericRepository
+    {
+        IEnumerable<T> Query<T>(string sql, object parameters);
+    }
+}
