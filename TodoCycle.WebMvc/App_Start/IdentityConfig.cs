30fc39fbfb77eec2e6ac6cc8849891f092f37499
ashes999 <ashes999@yahoo.com>
2017-04-20 15:04:20 -0400

    Fix unexpected compilation error

--- TodoCycle.WebMvc/App_Start/IdentityConfig.cs
+++ TodoCycle.WebMvc/App_Start/IdentityConfig.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using TodoCycle.WebMvc.Models;
+using Task = System.Threading.Tasks.Task;
 
 namespace TodoCycle.WebMvc
 {
